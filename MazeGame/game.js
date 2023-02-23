7d7b43da75ba2e847a4147af8b7c9ae5f83a4de0
HasaNoureddine <hasannoureddine351@gmail.com>
2023-02-23 23:36:54 +0200

    made code more compact

--- MazeGame/game.js
+++ MazeGame/game.js
@@ -2,15 +2,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let score = 0;
   let scoreboard = document.getElementsByClassName("boundary example");
   scoreboard[0].innerHTML = `score: ${score}`;
-
   let status = document.getElementById("status");
   let doc = document.getElementsByClassName("boundary");
+
   doc[0].addEventListener("mouseover", Lose);
   doc[1].addEventListener("mouseover", Lose);
   doc[2].addEventListener("mouseover", Lose);
   doc[3].addEventListener("mouseover", Lose);
   doc[4].addEventListener("mouseover", Lose);
 
+  //if we lose
   function Lose() {
     for (let i = 0; i < doc.length - 1; i++) {
       doc[i].style.backgroundColor = "red";
@@ -20,20 +21,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
     scoreboard[0].innerHTML = `score: ${score}`;
   }
 
-  let btn = document.getElementById("start");
-  btn.addEventListener("click", reset);
-  function reset() {
+  // reset
+  document.getElementById("start").addEventListener("click", function () {
     status.innerHTML = `Begin by moving your mouse over the "S"`;
     for (let i = 0; i < doc.length - 1; i++) {
       doc[i].style.backgroundColor = "rgb(238, 238, 238)";
     }
-  }
+  });
 
-  let btn2 = document.getElementById("end");
-  btn2.addEventListener("mouseover", win);
-  function win() {
+  //if we win
+  document.getElementById("end").addEventListener("mouseover", function () {
     status.innerHTML = "YOU WIN";
     score += 5;
     scoreboard[0].innerHTML = `score: ${score}`;
-  }
+  });
 });
